f70fe2f3edec43e907185bf73b2a2aa7e09a79bd
Braydon Squire <guitarf1@hotmail.com>
2017-10-27 20:17:37 -0600

    added login view styling

--- my-project/src/components/Login.js
+++ my-project/src/components/Login.js
@@ -6,10 +6,10 @@ export default class Login extends Component {
         return (
             <div className='video-container'>  
 
-                <div className="left-box"></div>
+                {/* <div className="left-box"></div>
                 <div className="right-box"></div>
                 <div className="bottom-box"></div>
-                <div className="top-box"></div>
+                <div className="top-box"></div> */}
                                 
                                         
                                             <iframe id="video-background" width="1920" height="1080" autoPlay src="https://player.vimeo.com/video/240066990?autoplay=1&loop=1&title=0&byline=0&portrait=0"  frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
@@ -20,14 +20,14 @@ export default class Login extends Component {
                 
                {/* <img className="sass-box" src="https://xenimanyme.files.wordpress.com/2017/07/cropped-xeni-logo-8.png?w=1180&h=533" alt=''/> */}
 
-                    
-
-                <div className="flex column">
-                    <div className="title">
+               <div className="title">
                     XENI
                     </div>
+
+                <div className="flex column">
+                    
                     <div id="photo" className="login-button">
-                <a href={ process.env.REACT_APP_LOGIN }><button className="login">Login</button></a>
+                <a href={ process.env.REACT_APP_LOGIN }><button className="login">LOGIN</button></a>
                     </div>
                 </div>
             </div> 
